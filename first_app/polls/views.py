545959f79aab5dfe55f7cadb52b5c733a913d690
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-14 18:20:01 -0500

    Define vote functionality

--- first_app/polls/views.py
+++ first_app/polls/views.py
@@ -1,7 +1,8 @@
 import re
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
 
 def detail(request, question_id):
   question = get_object_or_404(Question, pk= question_id)
@@ -12,8 +13,18 @@ def results(request, question_id):
   return HttpResponse(response)
 
 def vote(request, question_id):
-  response = f"You're voting on question {question_id}"
-  return HttpResponse(response)
+  question = get_object_or_404(Question, pk= question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/details.html', {
+      'error_message': "You didn't select a choice",
+      'question': question,
+    })
+  else:
+    selected_choice.votes += 1;
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
