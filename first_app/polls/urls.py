f04bd63bdeeb2e2fbc0b1934900fca9be7c78cd8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-13 23:04:11 -0500

    Add namespace

--- first_app/polls/urls.py
+++ first_app/polls/urls.py
@@ -2,6 +2,7 @@ import imp
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:question_id>/', views.detail, name='detail'),
