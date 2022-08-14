56cc6f1e4d000dda56d81b8610e7f00aa2b12cf6
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-13 22:34:37 -0500

    Render a template instead of a python view

--- first_app/polls/views.py
+++ first_app/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def detail(request, question_id):
@@ -15,5 +15,8 @@ def vote(request, question_id):
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(output)
+  template = loader.get_template('polls/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return HttpResponse(template.render(context, request))
