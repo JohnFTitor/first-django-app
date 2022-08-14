32aaf47657ead71bdd98136bd5f4ac89e627662f
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-13 22:48:30 -0500

    Add details page

--- first_app/polls/views.py
+++ first_app/polls/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+import re
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Question
 
 def detail(request, question_id):
-  return HttpResponse(f"You're looking at question {question_id}")
+  try:
+    question = Question.objects.get(pk= question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist!")
+  return render(request, "polls/detail.html", {'question': question})
 
 def results(request, question_id):
   response = f"You're looking at the results of question {question_id}"
